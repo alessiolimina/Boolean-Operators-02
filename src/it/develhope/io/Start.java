09e078c81ed5a935db5ba346abba94549bd3d3f4
Alessio Limina <alessio.limina@gmail.com>
2023-01-24 10:52:35 +0100

    Exercise: Boolean Operators 2

--- src/it/develhope/io/Start.java
+++ src/it/develhope/io/Start.java
@@ -1,2 +1,29 @@
-package it.develhope.io;public class Start {
+package it.develhope.io;
+
+public class Start {
+    public static void main(String[] args) {
+
+        System.out.println("---------Starting operations-----------------");
+
+        System.out.println("(55 != 55) && (false ^ true): " + ((55 != 55) && (false ^ true)));
+
+        System.out.println("------------------------------------");
+
+        boolean a = true;
+        boolean b = false;
+        int c = 2;
+        char d = '2';
+        System.out.println("Considering a=true, b=false,int c=2, char d='2':(!a || !b) || c == d: " + ((!a || !b) || c == d));
+
+        System.out.println("------------------------------------");
+
+        System.out.println("false && true || false && !false: " + (false && true || false && !false));
+
+        System.out.println("------------------------------------");
+
+        System.out.println("(false && true) || (false || true)): " + ((false && true) || (false || true)));
+
+        System.out.println("-------------------------------------");
+
+    }
 }
